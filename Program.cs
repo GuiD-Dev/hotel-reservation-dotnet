daa42f6c265e8a0f7337102241000ab810d5c2b8
Guilherme <guilhermed.dev@gmail.com>
2025-01-13 20:47:37 -0300

    feat(Program): adicionar rota default e rota de login para redirecionar para a página home

--- Program.cs
+++ Program.cs
@@ -1,6 +1,28 @@
+using System.Text.Json;
+using Microsoft.AspNetCore.Http.HttpResults;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseStaticFiles();
+app.UseCors(option => option.AllowAnyOrigin());
+
+app.MapGet("/", async context => { context.Response.Redirect("/login.html"); await Task.CompletedTask; });
+
+app.MapPost("/login", async context =>
+{
+  using var reader = new StreamReader(context.Request.Body);
+  var body = await reader.ReadToEndAsync();
+  var data = JsonSerializer.Deserialize<Dictionary<string, string>>(body);
+  var user = data["user"];
+  var password = data["password"];
+
+  context.Response.StatusCode = user == "admin" && password == "admin"
+    ? StatusCodes.Status200OK
+    : StatusCodes.Status401Unauthorized;
+});
 
 app.Run();
